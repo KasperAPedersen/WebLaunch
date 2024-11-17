be031127374835de8e93a84cb2a55a2a6b413e8b
Kasper Alexander Pedersen <kasperpedersen1999@gmail.com>
2024-11-17 11:28:46 +0100

    force redirect to https

--- app.js
+++ app.js
@@ -7,9 +7,18 @@ import * as fs from 'node:fs';
 const app = new Express();
 
 const options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
+    key: fs.readFileSync('/etc/letsencrypt/live/weblaunch.dk/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/weblaunch.dk/fullchain.pem')
 };
+
+app.enable('trust proxy');
+
+app.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production' && !req.secure) {
+        return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+});
 
 app.use(Express.static('Public'));
 
