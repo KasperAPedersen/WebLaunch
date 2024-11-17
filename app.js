bf83f989b8f4a8dc7a359ea789ede230d0d1a99b
Kasper Alexander Pedersen <kasperpedersen1999@gmail.com>
2024-11-17 10:39:27 +0100

    .

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 import Express from 'npm:express';
-import https from 'npm:https';
-import fs from 'npm:fs';
-
 const app = new Express();
 
 app.use(Express.static('Public'));
 
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
-
-https.createServer(options, app).listen(80, () => {
-    console.log('Server started on https://localhost:80');
+app.listen(3000, (e) => {
+    console.log(e ? e.message : 'Listening on port 3000');
 });
